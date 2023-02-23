0c3dcf58f9176a01872a6c0016729f7c079aba9e
Kichigin Grigoriy <gri12388@yandex.ru>
2023-02-24 00:06:00 +0400

    feat: сделал маску телефона

--- src/components/order/InputFormField.vue
+++ src/components/order/InputFormField.vue
@@ -6,6 +6,7 @@
       :name="name"
       :placeholder="placeholder"
       v-model="cmpValue"
+      ref="el"
     >
   </FormField>
 </template>
@@ -15,6 +16,7 @@ import {
   computed,
   defineProps,
   defineEmits,
+  ref,
 } from 'vue';
 
 import FormField from '@/components/order/FormField.vue';
@@ -35,8 +37,18 @@ type Emits = {
 const props = defineProps<Props>();
 const emit = defineEmits<Emits>();
 
+const el = ref<HTMLInputElement | null>(null);
+
 const cmpValue = computed({
   get: () => props.value,
-  set: (value: string) => emit('update:value', value),
+  set: (value: string) => {
+    const regexps = [/^$/, /^[+\d]$/, /^[+\d][\d]{0,15}$/];
+    if (props.type !== 'tel') emit('update:value', value);
+    else if (regexps.find((item) => item.test(value))) {
+      let valueCopy = value;
+      if (!regexps[0].test(valueCopy) && valueCopy[0] !== '+') valueCopy = `+${valueCopy}`;
+      emit('update:value', valueCopy);
+    } else if (el.value) el.value.value = props.value;
+  },
 });
 </script>
